90b1cd8856bd7dd6b514d77567a0fdb4380f5bb1
Juan Francisco Lorusso <juanflorusso@gmail.com>
2026-09-14 15:09:30 +0000

    fix: asignacion de input.Amount en NewRefundTransaction

--- internal/core/services/dispute_service.go
+++ internal/core/services/dispute_service.go
@@ -179,7 +179,7 @@ func (s *disputeService) ResolveDispute(ctx context.Context, input ports.Resolve
 		dispute.ReporterID,
 		dispute.ID,
 		dispute.BookingID,
-		dispute.DisputedAmount,
+		input.Amount,
 		0.0,
 		domain.RefundType("FULL"),
 		input.AdminNotes,
